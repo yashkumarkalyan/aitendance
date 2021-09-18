9fc0cf549c46d8853a7929927fad6f92616383e0
Yash Kumar Kalyan <82755153+yashkumarkalyan@users.noreply.github.com>
2021-09-18 19:27:49 +0530

    Added images data
    of all other 3 bhailogs

--- script.js
+++ script.js
@@ -43,21 +43,13 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = [
-    "Divyanshu",
-    "Captain America",
-    "Captain Marvel",
-    "Hawkeye",
-    "Jim Rhodes",
-    "Thor",
-    "Tony Stark",
-  ];
+  const labels = ["Akshat", "Divyanshu", "Pancham", "Ocean Negi"];
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
         const img = await faceapi.fetchImage(
-          `https://raw.githubusercontent.com/yashkumarkalyan/aitendance/blob/main/labeled_images/${label}/${i}.jpg`
+          `https://raw.githubusercontent.com/yashkumarkalyan/aitendance/main/labeled_images/${label}/${i}.jpg`
         );
         const detections = await faceapi
           .detectSingleFace(img)
